e5b698aefd92ae0221562295560dc83dc8d3f495
lftobs <tobbytobs1@gmail.com>
2025-02-15 00:58:55 +0100

    feat: Event ticket genrator app

---
+++ src/components/Header.jsx
@@ -0,0 +1,37 @@
+import logo from "../assets/logo.svg";
+import { ArrowRight } from "lucide-react";
+
+const Header = () => {
+		return (
+				<header>
+					<div>
+						<img src={logo} alt="logo" className="logo" />
+					</div>
+
+					<div className="nav">
+						<ul>
+							<li>
+								<a href="#">Events</a>
+							</li>
+
+							<li>
+								<a href="#">My Tickets</a>
+							</li>
+
+							<li>
+								<a href="#">About Project</a>
+							</li>
+						</ul>
+					</div>
+
+					<div>
+						<button className="header__btn">
+							my tickets
+							<ArrowRight className="btn__icon" />
+						</button>
+					</div>
+				</header>
+		)
+}
+
+export default Header
